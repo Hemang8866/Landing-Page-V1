93edec3f87c999fdc919d4598669a111e564a3c9
Hemang8866 <hemangkoshiyar112@gmail.com>
2025-02-09 20:54:18 +0530

    Footer section added

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,40 @@
+import Logo from "@/assets/icons/Logo.svg?react";
+import Facebook from "@/assets/icons/Facebook.svg?react";
+import Instagram from "@/assets/icons/Instagram.svg?react";
+import Linkedin from "@/assets/icons/Linkedin.svg?react";
+
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  const socialIconClass = "flex justify-center logo-social-light w-40 h-40";
+  return (
+    <footer className="flex flex-col gap-y-[36px] bg-neutral-90 p-24">
+      <div className="flex flex-col gap-y-16 justify-between items-center">
+        <span className="logo-light">
+          <Logo />
+        </span>
+        <div className="flex gap-x-24 items-center">
+          <a href="#">FAQ</a>
+          <a href="#">Privacy</a>
+          <a href="#">Support</a>
+          <a href="#">Contact</a>
+        </div>
+        <div className="flex gap-x-8 items-center">
+          <a href="#" className={socialIconClass}>
+            <Facebook />
+          </a>
+          <a href="#" className={socialIconClass}>
+            <Instagram />
+          </a>
+          <a href="#" className={socialIconClass}>
+            <Linkedin />
+          </a>
+        </div>
+      </div>
+      <span className="text-neutral-30 text-caption leading-caption font-normal text-center">
+        © {new Date().getFullYear()} Central Texas Fly Fishing All Rights
+        Reserved.
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/ui/sections/MemberStories/index.tsx
+++ src/components/ui/sections/MemberStories/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const MemberStories = () => {
   return <div>MemberStories</div>;
 };
