8d1b21293f4926f38eeb4e90ab40f8c3fc863c7d
Hemang8866 <hemangkoshiyar112@gmail.com>
2025-02-09 18:31:11 +0530

    base url added

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "/Landing-Page-V1/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
