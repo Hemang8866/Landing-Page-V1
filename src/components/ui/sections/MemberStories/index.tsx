9f6b4c099f6e55c009ebd143abdcbdd6c4c8611d
Hemang8866 <hemangkoshiyar112@gmail.com>
2025-02-09 21:39:36 +0530

    Member story section added

--- src/components/ui/sections/MemberStories/index.tsx
+++ src/components/ui/sections/MemberStories/index.tsx
@@ -1,5 +1,14 @@
-const MemberStories = () => {
-  return <div>MemberStories</div>;
+import React from "react";
+
+const MemberStories: React.FC = () => {
+  return (
+    <section className="flex flex-col gap-y-24 px-40 py-24">
+      <h1 className="text-defaultH leading-heading text-neutral-90 font-semibold">
+        Member stories
+      </h1>
+      <div className="flex flex-col divide-y-2 divide-neutral-20"></div>
+    </section>
+  );
 };
 
 export default MemberStories;
